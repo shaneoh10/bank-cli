fb0cfe4cf10f9a49d76fc7af7a8c41d1008e0467
Shane O'Hanlon <70384009+shaneoh10@users.noreply.github.com>
2026-05-08 16:59:24 +0900

    Add withdraw and deposit functions

--- bank.go
+++ bank.go
@@ -6,15 +6,25 @@ func main() {
 	var accountBalance = 1000.0
 
 	fmt.Println("Welcome to Go Bank!")
-	displayOptions()
 
-	var choice int
-	fmt.Print("Enter your choice: ")
-	fmt.Scanln(&choice)
+	for {
+		displayOptions()
 
-	if choice == 1 {
-		checkBalance(accountBalance)
+		var choice int
+		fmt.Print("Enter your choice: ")
+		fmt.Scanln(&choice)
+
+		if choice == 1 {
+			checkBalance(accountBalance)
+		} else if choice == 2 {
+			accountBalance = depositMoney(accountBalance)
+		} else if choice == 3 {
+			accountBalance = withdrawMoney(accountBalance)
+		} else {
+			break
+		}
 	}
+	fmt.Println("Thank you for using Go Bank. Goodbye!")
 }
 
 func displayOptions() {
@@ -27,4 +37,38 @@ func displayOptions() {
 
 func checkBalance(balance float64) {
 	fmt.Printf("Your balance is %.2f\n", balance)
+}
+
+func depositMoney(balance float64) float64 {
+	var depositAmount float64
+
+	fmt.Print("Enter amount to deposit: ")
+	fmt.Scanln(&depositAmount)
+
+	if depositAmount <= 0 {
+		fmt.Println("Invalid deposit amount.")
+		return balance
+	}
+
+	balance += depositAmount
+
+	fmt.Printf("You have deposited %.2f. Your new balance is %.2f\n", depositAmount, balance)
+
+	return balance
+}
+
+func withdrawMoney(balance float64) float64 {
+	var withdrawalAmount float64
+
+	fmt.Print("Enter amount to withdraw: ")
+	fmt.Scanln(&withdrawalAmount)
+
+	if withdrawalAmount <= balance {
+		balance -= withdrawalAmount
+		fmt.Printf("You have withdrawn %.2f. Your new balance is %.2f\n", withdrawalAmount, balance)
+	} else {
+		fmt.Println("Insufficient funds.")
+	}
+
+	return balance
 }
