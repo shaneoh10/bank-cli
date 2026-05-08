158ccf85e1dbcffe3d47eba7d951ccc8ee0d554d
Shane O'Hanlon <70384009+shaneoh10@users.noreply.github.com>
2026-05-08 16:30:35 +0900

    Set up balance check

--- bank.go
+++ bank.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var accountBalance = 1000.0
+
+	fmt.Println("Welcome to Go Bank!")
+	displayOptions()
+
+	var choice int
+	fmt.Print("Enter your choice: ")
+	fmt.Scanln(&choice)
+
+	if choice == 1 {
+		checkBalance(accountBalance)
+	}
+}
+
+func displayOptions() {
+	fmt.Println("What do you want to do?")
+	fmt.Println("1. Check Balance")
+	fmt.Println("2. Deposit Money")
+	fmt.Println("3. Withdraw Money")
+	fmt.Println("4. Exit")
+}
+
+func checkBalance(balance float64) {
+	fmt.Printf("Your balance is %.2f\n", balance)
 }
